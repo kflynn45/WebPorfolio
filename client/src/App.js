51132fc071189a6fa70d9b8b903f55ca0d60f7c8
Kevin Flynn <kevin@192.168.1.10>
2022-12-23 12:32:58 -0600

    Added Resume PDF Download

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
+
+const resumeDownload = () => {
+  fetch("Kevin Flynn's Resume Q1 2023.pdf").then(response => {
+    response.blob().then(blob => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        // Setting various property values
+        let alink = document.createElement('a');
+        alink.href = fileURL;
+        alink.download = "Kevin Flynn's Resume.pdf";
+        alink.click();
+    })
+  })
+}
 
 function App(){
   return(
-    <div>
+    <div class="mainHeader">
       <h1>Kevin Flynn</h1>
+      <button class="resumeButton" onClick={resumeDownload}>
+        Resume PDF Download
+      </button>
     </div>
   );
 }
